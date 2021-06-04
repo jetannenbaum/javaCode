18ce65ea7b04f553c0d627b0f9ece4f4a82915ba
Jim Tannenbaum <jetannenbaum@gmail.com>
2021-06-04 13:12:32 -0400

    Add files via upload

---
+++ Class2 - Sorts Exceptions and Data Structures/FileRead.java
@@ -0,0 +1,52 @@
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
+
+public class FileRead {
+    public static void main(String args[]) {
+        String filename = null;
+        String[] nameList = new String[6];
+        Scanner console = new Scanner(System.in);
+        Scanner textFile = null;
+        Integer idx = 0;
+        Boolean fileRead = false;
+
+        System.out.print("Enter the filename: ");
+        filename = console.nextLine();
+
+        try {
+            File file = new File(filename);
+            textFile = new Scanner(file);
+            for (int i = 0; i < nameList.length; i++) {
+                nameList[i] = textFile.nextLine();
+            }
+            fileRead = true;
+        }
+        catch (FileNotFoundException e) {
+            System.out.println(filename + " was not found");
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage() + " error while reading from file " + filename);
+        }
+        finally {
+            if (textFile != null) {
+                try {
+                    textFile.close();
+                }
+                catch (Exception e) {
+                    System.out.println("Error closing file");
+                }
+            }
+        }
+        console.close();
+
+        if (fileRead) {
+            System.out.println();
+            System.out.println("The " + nameList.length + " names");
+            for (int i = 0; i < nameList.length; i++) {
+                idx = i + 1;
+                System.out.println("Name[" + idx + "]: " + nameList[i]);
+            }
+        }
+    }
+}
